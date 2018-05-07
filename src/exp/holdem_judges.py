6aeff2020ce3e24e6ef27450d72b4732fe521ca8
vincci1988 <xun.bhsfer@utexas.edu>
2018-05-07 07:26:03 -0500

    holdem judge and unit tests

---
+++ src/exp/holdem_judges.py
@@ -0,0 +1,99 @@
+
+class Hand:
+    
+    def __init__(self, rank, cards):
+        self.rank = rank
+        self.cards = cards
+    
+    def __str__(self):
+        if self.rank == 0:
+            ans = 'High Card'
+        elif self.rank == 1:
+            ans = 'One Pair'
+        elif self.rank == 2:
+            ans = 'Two Pair'
+        elif self.rank == 3:
+            ans = 'Three of a Kind'
+        elif self.rank == 4:
+            ans = 'Straight'
+        elif self.rank == 5:
+            ans = 'Flush'
+        elif self.rank == 6:
+            ans = 'Full House'
+        elif self.rank == 7:
+            ans = 'Four of a Kind'
+        else:
+            ans = 'Straight Flush'
+        ans += ": " 
+        for card in self.cards:
+            ans += str(card)
+        return ans      
+
+
+def find_best_hand(cards):
+    cards.sort(key = lambda card: card.code)
+    fv = __flush_analyze(cards)
+    rv = __rank_analyze(cards)   
+    sf = __str8_analyze(fv)
+    if sf != None:
+        return Hand(8, sf)  #straight flush
+    if len(rv[0]) == 4:
+        if rv[0][0].getNumRank() == cards[0].getNumRank():
+            return Hand(7, rv[0] + cards[4 : 5]) # four of a kind
+        else:
+            return Hand(7, rv[0] + cards[0 : 1]) 
+    if len(rv[0]) == 3 and len(rv[1]) >= 2: 
+        return Hand(6, rv[0] + rv[1][0 : 2]) # full house
+    if fv != None:
+        return Hand(5, fv[0 : 5]) # flush
+    sv = __str8_analyze(cards) 
+    if sv != None:
+        return Hand(4, sv) # straight
+    if len(rv[0]) == 3:
+        return Hand(3, rv[0] + rv[1] + rv[2]) # three of a kind
+    if len(rv[0]) == 2 and len(rv[1]) == 2:
+        for card in cards:
+            r = card.getNumRank()
+            if r != rv[0][0].getNumRank() and r != rv[1][0].getNumRank():
+                return Hand(2, rv[0] + rv[1] + [card]) # two pair
+    if len(rv[0]) == 2:
+        return Hand(1, rv[0] + rv[1] + rv[2] + rv[3]) # one pair
+    return Hand(0, cards[:5]) # high card
+
+
+def __rank_analyze(cards):
+    rank_view = [[cards[0]]]
+    for j in range(1, len(cards)):
+        if cards[j].getNumRank() == rank_view[-1][0].getNumRank():
+            rank_view[-1].append(cards[j])
+        else:
+            rank_view.append([cards[j]])
+    rank_view.sort(key=lambda rank : len(rank), reverse = True)
+    return rank_view
+
+    
+def __flush_analyze(cards):
+    flush_view = [[], [], [], []]
+    for card in cards:
+        flush_view[card.getNumSuit()].append(card)
+    for suit in flush_view:
+        if len(suit) >= 5:
+            return suit
+    return None
+
+    
+def __str8_analyze(cards):
+    if cards == None:
+        return None
+    ans = [cards[0]]
+    for j in range(1, len(cards)):
+        if cards[j].getNumRank() - 1 == ans[-1].getNumRank():
+            ans.append(cards[j])
+            if len(ans) == 5:
+                return ans
+        elif cards[j].getNumRank() - 1 > ans[-1].getNumRank():
+            ans = [cards[j]]
+    if cards[0].getNumRank() == 0 and len(ans) == 4 and ans[-1].getNumRank() == 12:
+        ans.append(cards[0])
+        return ans
+    return None
---
+++ src/tests/UTest_holdem_judges.py
@@ -0,0 +1,130 @@
+
+import unittest
+from src.exp.holdem_judges import find_best_hand
+from src.exp.playing_cards import Card
+from math import floor
+
+
+class HoldemJudgeTest(unittest.TestCase):
+
+
+    def test_straight_flush(self):
+        case = "AdKdQdQcJdTsTd"
+        self.assertEqual("Straight Flush: AdKdQdJdTd", str(find_best_hand(str2cards(case))))
+        case = "Ad5s5h5d4d3d2d"
+        self.assertEqual("Straight Flush: 5d4d3d2dAd", str(find_best_hand(str2cards(case))))
+        case = "6s5s5h4s4d3s2s"
+        self.assertEqual("Straight Flush: 6s5s4s3s2s", str(find_best_hand(str2cards(case))))
+        
+        
+    def test_four_of_a_kind(self):
+        case = "AdTsThTdTc5d2d"
+        self.assertEqual("Four of a Kind: TsThTdTcAd", str(find_best_hand(str2cards(case))))
+        case = "KsTsJsKcQsKdKh"
+        self.assertEqual("Four of a Kind: KsKhKdKcQs", str(find_best_hand(str2cards(case))))
+        
+        
+    def test_full_house(self):
+        case = "KsQdJsQcQsKdKh"
+        self.assertEqual("Full House: KsKhKdQsQd", str(find_best_hand(str2cards(case))))
+        case = "KsAdThAcTsKdKh"
+        self.assertEqual("Full House: KsKhKdAdAc", str(find_best_hand(str2cards(case))))
+        case = "TsAdThAcTdKd7h"
+        self.assertEqual("Full House: TsThTdAdAc", str(find_best_hand(str2cards(case))))
+        case = "2sAd2h7c2dKd7h"
+        self.assertEqual("Full House: 2s2h2d7h7c", str(find_best_hand(str2cards(case))))
+        
+    
+    def test_flush(self):
+        case = "2sAd3s5s7sKs4h"
+        self.assertEqual("Flush: Ks7s5s3s2s", str(find_best_hand(str2cards(case))))
+        case = "2sAs9s5s7sKs4h"
+        self.assertEqual("Flush: AsKs9s7s5s", str(find_best_hand(str2cards(case))))
+        case = "2hAhTh5h7hKh4h"
+        self.assertEqual("Flush: AhKhTh7h5h", str(find_best_hand(str2cards(case))))
+    
+    def test_straight(self):
+        case = "2sAd3s5h7dKs4h"
+        self.assertEqual("Straight: 5h4h3s2sAd", str(find_best_hand(str2cards(case))))
+        case = "2sAd3s5h6dKs4h"
+        self.assertEqual("Straight: 6d5h4h3s2s", str(find_best_hand(str2cards(case))))
+        case = "KsAdQsThJd8s9h"
+        self.assertEqual("Straight: AdKsQsJdTh", str(find_best_hand(str2cards(case))))
+
+    def test_three_of_a_kind(self):
+        case = "3hAd3s5h3dKs4h"
+        self.assertEqual("Three of a Kind: 3s3h3dAdKs", str(find_best_hand(str2cards(case))))
+        case = "3hAd7s7h7d2s4h"
+        self.assertEqual("Three of a Kind: 7s7h7dAd4h", str(find_best_hand(str2cards(case))))
+        case = "KhJdKs5hKdTs4h"
+        self.assertEqual("Three of a Kind: KsKhKdJdTs", str(find_best_hand(str2cards(case))))
+    
+    def test_two_pair(self):
+        case = "KhJdJs5hKdTs4h"
+        self.assertEqual("Two Pair: KhKdJsJdTs", str(find_best_hand(str2cards(case))))
+        case = "KhJdJs5hKdTsTh"
+        self.assertEqual("Two Pair: KhKdJsJdTs", str(find_best_hand(str2cards(case))))
+        case = "AhJdJs5hKdTsTh"
+        self.assertEqual("Two Pair: JsJdTsThAh", str(find_best_hand(str2cards(case))))
+        case = "KhJdJs5h9d5s4h"
+        self.assertEqual("Two Pair: JsJd5s5hKh", str(find_best_hand(str2cards(case))))
+    
+    def test_one_pair(self):
+        case = "KhJdJs5hAd2sQh"
+        self.assertEqual("One Pair: JsJdAdKhQh", str(find_best_hand(str2cards(case))))
+        case = "7hJdJs5hAd2sQh"
+        self.assertEqual("One Pair: JsJdAdQh7h", str(find_best_hand(str2cards(case))))
+        case = "AhTdJs5hAd2sQh"
+        self.assertEqual("One Pair: AhAdQhJsTd", str(find_best_hand(str2cards(case))))
+    
+    def test_high_card(self):
+        case = "7hJd8s5hAd2sQh"
+        self.assertEqual("High Card: AdQhJd8s7h", str(find_best_hand(str2cards(case))))
+        case = "7hJh8s5hAd9sQh"
+        self.assertEqual("High Card: AdQhJh9s8s", str(find_best_hand(str2cards(case))))
+
+
+def str2cards(s):
+    ans =[]
+    for j in range(0, floor(len(s) / 2)):
+        ans.append(Card(str2code(s[2 * j : 2 * j + 2])))
+    return ans
+
+
+def str2code(s):
+    if s[0] == 'A':
+        rank = 0
+    elif s[0] == 'K':
+        rank = 1
+    elif s[0] == 'Q':
+        rank = 2
+    elif s[0] == 'J':
+        rank = 3         
+    elif s[0] == 'T':
+        rank = 4
+    elif s[0] == '9':
+        rank = 5
+    elif s[0] == '8':
+        rank = 6
+    elif s[0] == '7':
+        rank = 7
+    elif s[0] == '6':
+        rank = 8
+    elif s[0] == '5':
+        rank = 9
+    elif s[0] == '4':
+        rank = 10
+    elif s[0] == '3':
+        rank = 11
+    else:
+        rank = 12
+    code = rank * 4
+    if s[1] == 'h':
+        code += 1
+    elif s[1] == 'd':
+        code += 2
+    elif s[1] == 'c':
+        code += 3
+    return code
+        
+
